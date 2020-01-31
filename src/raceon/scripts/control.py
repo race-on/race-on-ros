7fe12734189495fb417389f5e38d89810c00c565
Valeriu Balaban <vbalaban@usc.edu>
2020-01-31 04:33:57 +0000

    Modify parameters for control

--- src/raceon/scripts/control.py
+++ src/raceon/scripts/control.py
@@ -12,21 +12,21 @@ SERVO_MAX = 900
 class Controller():
     
     def __init__(self):
-        self.topic_name_pos = rospy.get_param("topic_name_position", "position")
+        self.topic_name_pos_err = rospy.get_param("topic_name_position_error", "position/error")
         self.topic_name_control = rospy.get_param("topic_name_control", "control")
         
         # Parameters for control
         self.motor_speed = rospy.get_param("~motor_speed", 200)
         self.target = rospy.get_param("~target", 0)
-        self.kp = rospy.get_param("~kp", 0.1)
+        self.kp = rospy.get_param("~kp", 1)
     
     def start(self):
-        self.sub_pos = rospy.Subscriber(self.topic_name_pos, Pose, self.pos_callback)
+        self.sub_pos_err = rospy.Subscriber(self.topic_name_pos_err, Pose, self.pos_err_callback)
         self.pub_control = rospy.Publisher(self.topic_name_control, AckermannDrive, queue_size=10)
         rospy.spin()
 
-    def pos_callback(self, pos_msg):
-        pos_err = pos_msg.position.x - self.target
+    def pos_err_callback(self, pos_err_msg):
+        pos_err = pos_err_msg.position.x - self.target
         
         rospy.loginfo("Current error: pos_err = " + str(pos_err))
         
