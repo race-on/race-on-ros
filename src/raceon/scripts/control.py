500c41a68778b218008cd8c09aa4500a8aeffcf7
Valeriu Balaban <vbalaban@usc.edu>
2020-02-02 19:59:20 +0000

    Fix wrong direction

--- src/raceon/scripts/control.py
+++ src/raceon/scripts/control.py
@@ -2,31 +2,31 @@
 # license removed for brevity
 
 import rospy
-from geometry_msgs.msg import Pose
-from raceon.msg import AckermannDrive
+from geometry_msgs.msg import Pose, Twist
 
-SERVO_MIN = -900
-SERVO_MIDDLE = 0
-SERVO_MAX = 900
+# TODO
+SERVO_MIN = 0
+SERVO_MIDDLE = 512
+SERVO_MAX = 1024
 
 class Controller():
     
     def __init__(self):
-        self.topic_name_pos_err = rospy.get_param("topic_name_position_error", "position/error")
+        self.topic_name_pos = rospy.get_param("topic_name_position", "position")
         self.topic_name_control = rospy.get_param("topic_name_control", "control")
         
         # Parameters for control
-        self.motor_speed = rospy.get_param("~motor_speed", 200)
+        self.motor_speed = rospy.get_param("~motor_speed", 100)
         self.target = rospy.get_param("~target", 0)
-        self.kp = rospy.get_param("~kp", 1)
+        self.kp = rospy.get_param("~kp", 0.1)
     
     def start(self):
-        self.sub_pos_err = rospy.Subscriber(self.topic_name_pos_err, Pose, self.pos_err_callback)
-        self.pub_control = rospy.Publisher(self.topic_name_control, AckermannDrive, queue_size=10)
+        self.sub_pos = rospy.Subscriber(self.topic_name_pos, Pose, self.pos_callback)
+        self.pub_control = rospy.Publisher(self.topic_name_control, Twist, queue_size=10)
         rospy.spin()
 
-    def pos_err_callback(self, pos_err_msg):
-        pos_err = pos_err_msg.position.x - self.target
+    def pos_callback(self, pos_msg):
+        pos_err = self.target - pos_msg.position.x
         
         rospy.loginfo("Current error: pos_err = " + str(pos_err))
         
@@ -35,12 +35,11 @@ class Controller():
         
         rospy.loginfo("Control command: servo_pos = " + str(servo_pos) + ", motor_speed = " + str(motor_speed))
         
-        control_msg = AckermannDrive()
-        control_msg.speed = motor_speed
-        control_msg.steering_angle = servo_pos
+        control_msg = Twist()
+        control_msg.linear.y = motor_speed
+        control_msg.angular.y = servo_pos
         self.pub_control.publish(control_msg)
         
-    # TODO: Implement PID
     def pid(self, error):
         return error * self.kp
 
